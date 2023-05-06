a8c93573a1432e9731220a945e14309764d0ca7c
Yahir Rodríguez <126041734+yajirxz@users.noreply.github.com>
2023-05-05 23:40:55 -0600

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //Ventana ventana = new Ventana("Layouts");
-        VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout Cuadricula");
+        //VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout Cuadricula");
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Farenheid");
     }
 }
